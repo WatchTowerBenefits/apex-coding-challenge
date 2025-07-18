caf3dd79aae4e217b2e4bab2b71b61cc410d7577
Risa Hall <risahall@threeflow.com>
2025-07-18 15:50:10 -0600

    generate user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/create'
+  get 'user/update'
+  get 'user/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
