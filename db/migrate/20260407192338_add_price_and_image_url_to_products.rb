14c4dee365c775f102edfe2e6188c903e84a5f19
Risa Hall <risahall@threeflow.com>
2026-04-09 15:49:16 -0600

    cleanup

--- db/migrate/20260407192338_add_price_and_image_url_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddPriceAndImageUrlToProducts < ActiveRecord::Migration[7.2]
-  def change
-    add_column :products, :price, :decimal, precision: 8, scale: 2
-    add_column :products, :image_url, :string
-  end
-end
