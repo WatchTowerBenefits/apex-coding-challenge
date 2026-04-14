c4cf120abe96b9f60de86e490f1c96fedfda1e6f
Risa Hall <risahall@threeflow.com>
2026-04-14 15:54:03 -0600

    set rails env to dev to enable hot reload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://localhost:3000',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
